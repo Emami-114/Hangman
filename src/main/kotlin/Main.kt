613fbbdad0d7705a3e51d5bfb77e2f62f3c9809b
Emami.114 <emami.114@outlook.de>
2023-01-23 17:29:00 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,106 @@
+import java.io.File
+val hangman = listOf("""
+    -----
+    |   |
+    |   o
+    |  /|\
+    |  / \
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |   |
+    |   o
+    |  /|\
+    |  / 
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |   |
+    |   o
+    |  /|\
+    |  
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |   |
+    |   o
+    |  /|
+    |  
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |   |
+    |   o
+    |  /
+    |
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |   |
+    |   o
+    |  
+    |  
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |   |
+    |   
+    |  
+    |  
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    |  
+    |   
+    |  
+    |  
+    |
+    ------
+""".trimIndent(),
+    """
+    -----
+    
+    
+    
+    
+    
+    ------
+""".trimIndent(),
+
+
+    )
+fun main() {
+    val word = File("word.txt").readLines().random()
+    var status = "_".repeat(word.length).toCharArray()
+    var life = hangman.lastIndex
+    while (String(status) != word && life > 0){
+        println(hangman[life-1])
+        println(status)
+        val input = readln()
+        if (input.length > 1 ){
+//            compare whole word
+            if (input == word) status = input.toCharArray()
+            else life--
+        }else if (input in word){
+            word.forEachIndexed { index, c ->
+                if (c in input) status[index] = c
+            }
+        }else life--
+    }
+    if (life > 0) println("Congratulation")
+    else println("Answer was $word")
+}
